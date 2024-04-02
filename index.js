bf65ef7552d4df1d74aeb3af866fd6fa9f2df8e8
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-04-02 21:42:10 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+// writting a function superbowlwin
+
+function superbowlwin(superBowls) {
+    for (let i= 0; i < superBowls.length; i++) {
+}
+//checknif the result property is 'win'
+if (superBowls[i].result === 'win') {
+    return superBowls[i].year;
+}
+}
+// if no win is found, return undefined
+return undefined;
+
+
+module.exports = superbowlwin;
